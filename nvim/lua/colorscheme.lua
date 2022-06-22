6539b4a6d105b874395dbb249760a2f998ea3877
GabrielRIP <jgrcreations@gmail.com>
2022-06-22 18:43:47 -0500

    se agrega la configuracion para cambiar el highlighting de NvimTree

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -3,3 +3,9 @@ vim.highlight.create('FloatBorder', { guibg='none' }, false)
 vim.highlight.create('WhichKeyFloat', { guibg='none' }, false)
 vim.highlight.create('BufferTabpageFill', { guibg='none' }, false)
 vim.highlight.create('VertSplit', { guibg='#16161e', guifg='#16161e' }, false)
+
+
+-- NOTE: color de Folder y FolderName de NvimTRee
+vim.cmd('highlight NvimTreeFolderIcon guifg=goldenrod')
+vim.cmd('highlight NvimTreeFolderName guifg=goldenrod')
+vim.cmd('highlight NvimTreeOpenedFolderName guifg=sienna')
