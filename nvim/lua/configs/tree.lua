5245352bfd647e48f5b5f74c0855b23a94850624
GabrielRIP <jgrcreations@gmail.com>
2022-06-22 18:42:50 -0500

    se documenta en espaniol :) & se depura codigo innecesario

--- nvim/lua/configs/tree.lua
+++ nvim/lua/configs/tree.lua
@@ -1,192 +1,144 @@
-local utils = require('utils')
-local nvim_tree_events = require('nvim-tree.events')
-local bufferline_state = require('bufferline.state')
+-- Import Icons del fichero /lua/icons.lua
+local icon_status_ok, icon = pcall(require, 'lua.icons')
+if not icon_status_ok then
+   return
+end
 
-local TREE_WIDTH = 33
-
-local git_icons = {
-  unstaged = '',
-  staged = '',
-  unmerged = '',
-  renamed = '➜',
-  untracked = '',
-  deleted = '',
-  ignored = '◌'
-}
+-- ╭──────────────────────────────────────────────────────────╮
+-- │  HACK: Variables de aceso rapido y keymaps custom        │
+-- ╰──────────────────────────────────────────────────────────╯
+local TREE_WIDTH = 33       -- Ancho de la ventana NvimTree
+local view_side = 'left'    -- direccion de la ventana NvimTree
 
 local keymappings = {
-  { key = {'<CR>', 'o', '<2-LeftMouse>'}, action = 'edit' },
-  -- <C-e> keymapping cannot be set because it's used for toggling nvim-tree
-  -- { key = '<C-e>',                        action = 'edit_in_place' },
-  { key = {'O'},                          action = 'edit_no_picker' },
-  { key = {'<2-RightMouse>', '<C-]>'},    action = 'cd' },
-  { key = '<C-v>',                        action = 'vsplit' },
-  { key = '<C-x>',                        action = 'split' },
-  { key = '<C-t>',                        action = 'tabnew' },
-  { key = '<',                            action = 'prev_sibling' },
-  { key = '>',                            action = 'next_sibling' },
-  { key = 'P',                            action = 'parent_node' },
-  { key = '<BS>',                         action = 'close_node' },
-  { key = '<Tab>',                        action = 'preview' },
-  { key = 'K',                            action = 'first_sibling' },
-  { key = 'J',                            action = 'last_sibling' },
-  { key = 'I',                            action = 'toggle_ignored' },
-  { key = 'H',                            action = 'toggle_dotfiles' },
-  { key = 'R',                            action = 'refresh' },
-  { key = 'a',                            action = 'create' },
-  { key = 'd',                            action = 'remove' },
-  { key = 'D',                            action = 'trash' },
-  { key = 'r',                            action = 'rename' },
-  { key = '<C-r>',                        action = 'full_rename' },
-  { key = 'x',                            action = 'cut' },
-  { key = 'c',                            action = 'copy' },
-  { key = 'p',                            action = 'paste' },
-  { key = 'y',                            action = 'copy_name' },
-  { key = 'Y',                            action = 'copy_path' },
-  { key = 'gy',                           action = 'copy_absolute_path' },
-  { key = '[c',                           action = 'prev_git_item' },
-  { key = ']c',                           action = 'next_git_item' },
-  { key = '-',                            action = 'dir_up' },
-  { key = 's',                            action = 'system_open' },
-  { key = 'q',                            action = 'close' },
-  { key = 'g?',                           action = 'toggle_help' },
-  { key = 'W',                            action = 'collapse_all' },
-  { key = 'S',                            action = 'search_node' }
+   { key = {'<CR>', 'o', '<2-LeftMouse>'}, action = 'edit' },
+   { key = {'O'},                          action = 'edit_no_picker' },
+   { key = {'<2-RightMouse>', '<C-]>'},    action = 'cd' },
+   { key = '<C-v>',                        action = 'vsplit' },
+   { key = '<C-h>',                        action = 'split' },
+   { key = 'P',                            action = 'parent_node' },
+   { key = '<BS>',                         action = 'close_node' },
+   { key = '<Tab>',                        action = 'preview' },
+   { key = 'K',                            action = 'first_sibling' },
+   { key = 'J',                            action = 'last_sibling' },
+   { key = 'I',                            action = 'toggle_ignored' },
+   { key = 'H',                            action = 'toggle_dotfiles' },
+   { key = 'R',                            action = 'refresh' },
+   { key = 'a',                            action = 'create' },
+   { key = 'd',                            action = 'remove' },
+   { key = 'D',                            action = 'trash' },
+   { key = 'r',                            action = 'rename' },
+   { key = '<C-r>',                        action = 'full_rename' },
+   { key = 'x',                            action = 'cut' },
+   { key = 'c',                            action = 'copy' },
+   { key = 'p',                            action = 'paste' },
+   { key = 'y',                            action = 'copy_name' },
+   { key = 'Y',                            action = 'copy_path' },
+   { key = 'gy',                           action = 'copy_absolute_path' },
+   { key = '[c',                           action = 'prev_git_item' },
+   { key = ']c',                           action = 'next_git_item' },
+   { key = '-',                            action = 'dir_up' },
+   { key = 's',                            action = 'system_open' },
+   { key = 'q',                            action = 'close' },
+   { key = 'g?',                           action = 'toggle_help' },
+   { key = 'W',                            action = 'collapse_all' },
+   { key = 'S',                            action = 'search_node' }
 }
 
+-- NOTE: Configuracion de NvimTree
 require'nvim-tree'.setup {
-  -- disables netrw completely
-  disable_netrw       = false,
-  -- hijack netrw window on startup
-  hijack_netrw        = true,
-  -- open the tree when running this setup function
-  open_on_setup       = false,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = {},
-  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = false,
-  -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = true, -- para tener el cursor un espacio después del nombre
-  sort_by = 'name',
-  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-  update_cwd          = true,
-  -- opens in place of the unnamed buffer if it's empty
-  hijack_unnamed_buffer_when_opening = false,
-  --false by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree
-  respect_buf_cwd = true,
-  -- show lsp diagnostics in the signcolumn
-  diagnostics         = {
-    enable = true,
-    icons  = {
-      hint    = '',
-      info    = '',
-      warning = '',
-      error   = '',
-    }
-  },
-  renderer = {
-    add_trailing = true, -- agrega / al final del folder
-    group_empty = true,
-    highlight_git = true,
-    highlight_opened_files = 'none',
-    root_folder_modifier = table.concat { ':t:gs?$?/..', string.rep('            ', 1), string.rep(' ', 500), '?:gs?^??' },
-    indent_markers = {
-      enable = true,
-      icons = {
-        corner = '╰ ',
-        edge = '│ ',
-        none = '  ',
-      },
-    },
-    icons = {
-      glyphs = {
-        git = git_icons
+   open_on_tab        = false,            -- Abre el arbol cuando se cambia de buffer o se abro uno
+   hijack_cursor      = true,             -- para tener el cursor un espacio después del nombre
+   sort_by            = 'name',           -- Como se ordena en directorio: 'name', 'case_sensitive', 'modification_time', 'extension'
+   update_cwd         = true,             -- Cambiar el directorio raiz del arbol en DirChanged
+   respect_buf_cwd    = true,             -- Cambiar el CWD de NvimTree al nuevo buffer al abrir NvimTree
+
+   diagnostics = {                        -- Diagnosticos del LSP
+      enable = true,                      -- Habilitar el diagnostico
+      icons  = {                          -- Iconos Custom
+         hint    = icon.hint,
+         info    = icon.info,
+         warning = icon.warningTriangle,
+         error   = icon.error,
       }
-    }
-  },
-  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-  update_focused_file = {
-    -- enables the feature
-    enable      = true,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = true,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = {}
-  },
-  -- configuration options for the system open command (`s` in the tree by default)
-  system_open = {
-    -- the command to run this, leaving nil should work in most cases
-    cmd  = '',
-    -- the command arguments as a list
-    args = {}
-  },
-  filters = {
-    dotfiles = false,
-    custom = {
-            '.swp', '.pyc', '.git', 'node_modules', '.cache',
-            '.watchmanconfig', '.ruby-version', 'Gemfile', '.flowconfig',
-            'buckconfig', '.bundle', '__tests__', 'style.css.map'
-    }
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
-    open_file = {
-      quit_on_open = true, -- para cerrar el administrador de archivos al seleccionar uno.
-      -- if true the tree will resize itself after opening a file
-      resize_window = true,
-      window_picker = {
-        enable = true,
-        chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
-        exclude = {
-          filetype = { 'notify', 'packer', 'qf', 'diff', 'fugitive', 'fugitiveblame' },
-          buftype = { 'nofile', 'terminal', 'help' },
-        },
+   },
+   renderer = {
+      add_trailing = true,                -- Agrega '/' al final del folder
+      group_empty = false,                -- Compactar Carpetas que solo tienen una carpeta, Problema al agregar o renombrar, por eso lo puse false
+      highlight_git = true,               -- Habilitar el resaltado de Git.
+      highlight_opened_files = 'all',     -- Resaltado del archivo si este esta abierto. 'none', 'icon', 'name', 'all'
+      root_folder_modifier = table.concat { ':t:gs?$?/..', string.rep('            ', 1), string.rep(' ', 500), '?:gs?^??' },
+      indent_markers = {                  -- Indentado
+         enable = true,                   -- Habilitar el indentado
+         icons = {                        -- Iconos para el indentado Custom
+            corner = '╰ ',
+            edge = '│ ',
+            none = '  ',
+         },
       },
-    },
-  },
-  view = {
-    -- width of the window, can be either a number (columns) or a string in `%`
-    width = TREE_WIDTH,
-    hide_root_folder = false,
-    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-    side = 'left',
-    mappings = {
-      -- custom only false will merge the list with the default mappings
-      -- if true, it will only use your list to set the mappings
-      custom_only = true,
-      -- list of mappings to set on the tree manually
-      list = keymappings
-    },
-    number = false,
-    relativenumber = false
-  },
-  trash = {
-    cmd = 'trash',
-    require_confirm = true
-  }
+      icons = {                           -- Iconos de carpetas, Git
+         glyphs = {
+            git = {                       -- Iconos de estados de Git
+               unstaged = icon.gitChange,
+               staged = icon.checkSquare,
+               unmerged = icon.gitUnmerged,
+               renamed = icon.rename,
+               untracked = icon.gitAdd,
+               deleted = icon.gitRemove,
+               ignored = icon.gitIgnore,
+            },
+         }
+      }
+   },
+   update_focused_file = {
+      enable      = true,                 -- Actualizar el archivo enfocado en 'BufEnter'
+   },
+   filters = {
+      dotfiles = false,                   -- Nom mostrar archivos ocultos, Alternar con 'H' -> toggle_dotfiles
+      custom = {                          -- Filtra archivos o ficheros
+         '.swp', '.pyc', '.git', 'node_modules', '.cache',
+         '.watchmanconfig', '.ruby-version', 'Gemfile', '.flowconfig',
+         'buckconfig', '.bundle', '__tests__', 'style.css.map'
+      }
+   },
+   git = {
+      enable = true,                      -- Habilita el uso de git
+      ignore = true,                      -- Ignora los archivos dentro de '.gitignore' require git.enable = true
+      timeout = 500,                      -- Mata el proceso de git si tarda demasiado, default 400ms
+   },
+   actions = {
+      use_system_clipboard = true,        -- Para usar el portapales
+      change_dir = {
+         enable = true,                   -- Cambiar el directorio de trabajo al bambiar de directorio
+         global = true,                   -- cambiar de directorio con :cd
+         restrict_above_cwd = true,       -- Restringir cambiar a un directorio superior
+      },
+      open_file = {
+         quit_on_open = true,             -- Cierra la ventana de NvimTree al seleccionar un elemento
+         resize_window = true,            -- Redimencionar Ventana
+         window_picker = {
+            enable = true,                -- Para abrir archivos en otro buffer diferente al de NvimTree
+            chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890',
+            exclude = {                   -- No seleccionar la lista
+               filetype = { 'notify', 'packer', 'qf', 'diff', 'fugitive', 'fugitiveblame' },
+               buftype = { 'nofile', 'terminal', 'help' },
+            },
+         },
+      },
+   },
+   view = {
+      width = TREE_WIDTH,                 -- Ancha de la ventana
+      hide_root_folder = false,           -- Ocultar la ruta de trabajo actual
+      side = view_side,                   -- El dado del panel NvimTree
+      mappings = {
+         custom_only = true,              -- Mapeo Personalizado
+         list = keymappings               -- configuracion de maps
+      },
+      number = false,                     -- Para agrear numero de lineas
+      relativenumber = false              -- Un numero relativo a la linea en focus
+   },
+   trash = {
+      cmd = 'trash',                      -- Comando para eliminal elementos, default=>'gio trash'
+      require_confirm = true              -- Aviso para aceptar la eliminacion del elemento.
+   }
 }
-
--- color del icono del folder
-vim.cmd('highlight NvimTreeFolderIcon guifg=goldenrod')
-vim.cmd('highlight NvimTreeFolderName guifg=sienna')
-vim.cmd('highlight NvimTreeOpenedFolderName guifg=goldenrod')
-
---nvim_tree_events.on_tree_open(function ()
---    bufferline_state.set_offset(TREE_WIDTH + 1, utils.add_whitespaces(13) .. 'File Explorer')
---end)
-
---nvim_tree_events.on_tree_close(function ()
- --   bufferline_state.set_offset(0)
---end)
