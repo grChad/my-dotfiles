7fc9fdfa66ab32ff7cfdbd5d6d9b110f7be40806
GabrielRIP <jgrcreations@gmail.com>
2022-06-22 19:00:27 -0500

    se retorna la config de highlighting de NvimTree al fichero tree.lua

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -3,9 +3,3 @@ vim.highlight.create('FloatBorder', { guibg='none' }, false)
 vim.highlight.create('WhichKeyFloat', { guibg='none' }, false)
 vim.highlight.create('BufferTabpageFill', { guibg='none' }, false)
 vim.highlight.create('VertSplit', { guibg='#16161e', guifg='#16161e' }, false)
-
-
--- NOTE: color de Folder y FolderName de NvimTRee
-vim.cmd('highlight NvimTreeFolderIcon guifg=goldenrod')
-vim.cmd('highlight NvimTreeFolderName guifg=goldenrod')
-vim.cmd('highlight NvimTreeOpenedFolderName guifg=sienna')
