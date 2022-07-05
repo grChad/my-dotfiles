d3ff0cd2270e963c32ce22c6c7a9641ace7d7499
GabrielRIP <jgrcreations@gmail.com>
2022-07-05 04:39:06 +0000

    se mejora el tema de colores

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,5 +1,19 @@
+require('onedark').setup({
+   dark_float = false,                -- Ventanas flotantes mas oscuras
+   hide_inactive_statusline = true,   -- Nvim Tree background, mas oscuro
+   dark_sidebar = true,               -- Linea de numeros y signos mas oscuras
+   highlight_linenumber = true,       -- linea de numeros y signos mas oscuras.
+
+   overrides = function ()
+      return {
+         Visual = { style = 'inverse' }
+      }
+   end,
+   dev = true,
+})
+
 vim.highlight.create('NormalFloat', { guibg='none', guifg='none' }, false)
 vim.highlight.create('FloatBorder', { guibg='none' }, false)
 vim.highlight.create('WhichKeyFloat', { guibg='none' }, false)
 vim.highlight.create('BufferTabpageFill', { guibg='none' }, false)
-vim.highlight.create('VertSplit', { guibg='#16161e', guifg='#16161e' }, false)
+vim.highlight.create('VertSplit', { guifg='#5C6370', guibg='#21252B' }, false)
--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,16 +1,2 @@
 -- Para que los comentarios no se repitan
 vim.cmd([[ autocmd! BufEnter * set fo-=r fo-=o ]])
-
-require('onedark').setup({
-   dark_float = false,                -- Ventanas flotantes mas oscuras
-   hide_inactive_statusline = true,   -- Nvim Tree background, mas oscuro
-   dark_sidebar = true,               -- Linea de numeros y signos mas oscuras
-   highlight_linenumber = true,       -- linea de numeros y signos mas oscuras.
-
-   overrides = function ()
-      return {
-         Visual = { style = 'inverse' }
-      }
-   end,
-   dev = true,
-})
