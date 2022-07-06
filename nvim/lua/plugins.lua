8a9a23997a3c8371dc0720b5c32e9e2d17541f19
GabrielRIP <jgrcreations@gmail.com>
2022-07-06 14:20:46 -0500

    se ordena y documenta mejor

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -11,86 +11,57 @@ end
 
 return require('packer').startup({
    function(use)
-      -- Packer plugin manager --------------------------------------------------
+      -- (1) Packer plugin manager
       use { "wbthomason/packer.nvim" }
-
-      -- Theme for Nvim ---------------------------------------------------------
-      use { 'ful1e5/onedark.nvim' }
-
-      -- Dependencies -----------------------------------------------------------
-      use {
+-- +--------------------------------------------------------------------+
+      -- (2) Necesarios para cargar primero
+      use { 'lewis6991/impatient.nvim' }
+      use { 'nathom/filetype.nvim'}
+      use { 'nvim-lua/plenary.nvim' }
+      use { -- iconos de archivos
          'kyazdani42/nvim-web-devicons',
          config = function () require ('configs.web-devicons') end
       }
-      use {
-         'lewis6991/impatient.nvim',
-         config = function () require('impatient').enable_profile() end
+      use { -- Para la portada inicial
+         'goolord/alpha-nvim',
+         config = function() require('configs.alpha') end
       }
-
-      -- syntax Highlighting ----------------------------------------------------
+-- +--------------------------------------------------------------------+
+      -- (3) Themes
+      use { 'ful1e5/onedark.nvim' }
+-- +--------------------------------------------------------------------+
+      -- (4) Treesitter
       use {
          'nvim-treesitter/nvim-treesitter',
          config = function() require ('configs.treesitter') end
       }
-
-      -- Buffer y barra de estado -----------------------------------------------
-      use { -- Buffer
-         'romgrk/barbar.nvim',
-         requires = { 'kyazdani42/nvim-web-devicons' },
-         config = function() require('configs.barbar') end
+      use {
+         'nvim-treesitter/nvim-treesitter-textobjects',
+         after = 'nvim-treesitter'
       }
-      use { -- State Bar
-         'nvim-lualine/lualine.nvim',
-         requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-         config = function() require ('configs.lualine') end
+      use {
+         'RRethy/nvim-treesitter-textsubjects',
+         after = 'nvim-treesitter'
       }
-
-      -- Tree file manager ------------------------------------------------------
+      use {
+         'm-demare/hlargs.nvim',
+         config = function() require('hlargs').setup() end
+      }
+-- +--------------------------------------------------------------------+
+      -- (5) Navegacion (Telescope/ Tree/ Refactor)
       use {
          'kyazdani42/nvim-tree.lua',
          requires = { 'kyazdani42/nvim-web-devicons' },
          config = function() require ('configs.tree') end,
       }
-
-      -- IDE and complementos ---------------------------------------------------
+-- +--------------------------------------------------------------------+
+      -- (6) LSP Base
       use {
-         'windwp/nvim-autopairs',
-         after = { 'nvim-treesitter', 'nvim-cmp' },
-         config = function() require('configs.autopairs') end
+         'neovim/nvim-lspconfig',
+         config = function() require ('lsp.lspconfig') end
       }
-      use { 'easymotion/vim-easymotion' }
-      use { 'editorconfig/editorconfig-vim' }   -- normalize files
-      use { 'KabbAmine/vCoolor.vim' }           -- palet of colors
-      use { -- Para moverse por las linear buscando en el cmd por :Number
-         'nacro90/numb.nvim',
-         config = function() require('numb').setup() end
-      }
-      use { -- indentado en lineas
-         'lukas-reineke/indent-blankline.nvim',
-         config = function() require ('configs.blankline') end
-      }
-
-      use { -- commentary
-         'numToStr/Comment.nvim',
-         config = function () require('Comment').setup() end,
-      }
-      use { 'LudoPinelli/comment-box.nvim' }
-      use { -- comentarios con mas detalles
-         'folke/todo-comments.nvim',
-         config = function() require('configs.todo-comments') end
-      }
-
-      use { -- sigs for git
-         'lewis6991/gitsigns.nvim',
-         config = function () require ('configs.gitsigns') end
-      }
-      use { -- pigment of colors
-         'norcalli/nvim-colorizer.lua',
-         config = function () require ('configs.colorizer') end
-      }
-      use { 'airblade/vim-rooter' }
-
-      -- lsp and cmp ------------------------------------------------------------
+-- +--------------------------------------------------------------------+
+      -- (7) CMP
       use {
          'hrsh7th/nvim-cmp', -- Autocompletion plugin
          requires = {
@@ -102,20 +73,68 @@ return require('packer').startup({
          },
          config = function() require ('lsp.cmp') end
       }
-
-      use { -- Collection of configurations for built-in LSP client
-         'neovim/nvim-lspconfig',
-         config = function() require ('lsp.lspconfig') end
-      }
-
-      -- LSP Addons
+-- +--------------------------------------------------------------------+
+      -- (8) LSP Complementos
       use {
          'stevearc/dressing.nvim',
          requires = { 'MunifTanjim/nui.nvim' },
          config = function() require ('configs.dressing') end
       }
-
-      -- Snippets ---------------------------------------------------------------
+-- +--------------------------------------------------------------------+
+      -- (9) Buffer and Bar state
+      use { -- Buffer
+         'romgrk/barbar.nvim',
+         requires = { 'kyazdani42/nvim-web-devicons' },
+         config = function() require('configs.barbar') end
+      }
+      use { -- State Bar
+         'nvim-lualine/lualine.nvim',
+         requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+         config = function() require ('configs.lualine') end
+      }
+-- +--------------------------------------------------------------------+
+      -- (10) General
+      use { 'AndrewRadev/switch.vim' }
+      use { -- commentary
+         'numToStr/Comment.nvim',
+         config = function () require('Comment').setup() end,
+      }
+      use {
+         'LudoPinelli/comment-box.nvim',
+         config = function() require('configs.comment_box') end
+      }
+      use { -- comentarios con mas detalles
+         'folke/todo-comments.nvim',
+         config = function() require('configs.todo-comments') end
+      }
+      use { 'easymotion/vim-easymotion' }
+      use { 'airblade/vim-rooter' }
+-- +--------------------------------------------------------------------+
+      -- (11) Snippets & Language & syntax
+      use {
+         'windwp/nvim-autopairs',
+         after = { 'nvim-treesitter', 'nvim-cmp' },
+         config = function() require('configs.autopairs') end
+      }
+      use {
+         'windwp/nvim-ts-autotag',
+         config = function() require('nvim-ts-autotag').setup() end
+      }
+      use { 'p00f/nvim-ts-rainbow', after = { 'nvim-treesitter' } }
+      use { -- indentado en lineas
+         'lukas-reineke/indent-blankline.nvim',
+         config = function() require ('configs.blankline') end
+      }
+      use { -- pigment of colors
+         'norcalli/nvim-colorizer.lua',
+         config = function () require ('configs.colorizer') end
+      }
+      use { 'KabbAmine/vCoolor.vim' }           -- palet of colors
+      use { 'editorconfig/editorconfig-vim' }   -- normalize files
+      use { -- Para moverse por las linear buscando en el cmd por :Number
+         'nacro90/numb.nvim',
+         config = function() require('numb').setup() end
+      }
       use { 'rafamadriz/friendly-snippets' }
       use {
          "L3MON4D3/LuaSnip",
@@ -123,13 +142,25 @@ return require('packer').startup({
             require("luasnip.loaders.from_vscode").lazy_load()
          end
       }
-
-      -- Para umular ficheros markdown ------------------------------------------
+-- +--------------------------------------------------------------------+
+      -- (12) Git
+      use { -- sigs for git
+         'lewis6991/gitsigns.nvim',
+         requires = { 'nvim-lua/plenary.nvim' },
+         config = function () require ('configs.gitsigns') end,
+         event = 'BufRead'
+      }
+      use {
+         'akinsho/git-conflict.nvim',
+         config = function() require('git-conflict').setup() end
+      }
+-- +--------------------------------------------------------------------+
+      -- (13) Preview MarkDown
       use {
          'iamcco/markdown-preview.nvim',
          run = 'cd app && npm install'
       }
-
+-- +--------------------------------------------------------------------+
       if packer_bootstrap then
          require('packer').sync()
       end
