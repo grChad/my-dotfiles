d85c18f4b8a43a7ef74fe001ba2080b7a804e12e
GabrielRIP <jgrcreations@gmail.com>
2022-07-06 19:29:09 -0500

    se documenta muy bien

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,22 @@
--- ╭──────────────────────────────────────────────────────────╮
--- │      init.lua by Gabriel Rivera -> GabrielRIP            │
--- │      > nvim la mas actual y stable                       │
--- │      > Language Servers: comprobar lsp y linters         │
--- ╰──────────────────────────────────────────────────────────╯
+-- ┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┓
+-- ╏              init.lua by Gabriel Rivera -> GabrielRIP              ╏
+-- ╏                   > nvim la mas actual y stable                    ╏
+-- ╏            > Language Servers: comprobar lsp y linters             ╏
+-- ┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┛
 
 -- PERFORMANCE: Acelera la carga de modulos Lua en Nvim
 local impatient_ok, impatient = pcall(require, 'impatient')
 if impatient_ok then impatient.enable_profile() end
-
+-- +--------------------------------------------------------------------+
 -- PERFORMANCE: Para que no sea compatible con 'vi', pero no de golpe
 if vim.opt.compatible == true then vim.opt.compatible = false end
 
-require('packer_commands')       -- Comandos para Packer & call plugins
-require('settings')              -- configuracion de sets
-require('colorscheme')           -- Modificar la visualizacion del IDE
-require('mappings')              -- atajos de teclado.
-vim.cmd('source $HOME/.config/nvim/lua/keys.vim')
-require('autocmds')              -- Auto-comandos.
-require 'utils' -- others
-
--- Colour overrides
-vim.cmd([[
-	highlight! link CursorLineNr MatchParen
-	highlight! link WinSeparator LineNr
-]])
+-- ╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┓
+require('packer_commands')       -- Comandos para Packer & call plugins ╏
+require('settings')              -- configuracion de sets               ╏
+require('colorscheme')           -- Modificar la visualizacion del IDE  ╏
+require('mappings')              -- atajos de teclado.                  ╏
+vim.cmd('source $HOME/.config/nvim/lua/keys.vim') -- por adaptar        ╏
+require('autocmds')              -- Auto-comandos.                      ╏
+require 'utils'                  -- others                              ╏
+-- ╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┛
