f62c74e05b9c42baec59b57e689519dff18c2544
GabrielRIP <jgrcreations@gmail.com>
2022-07-06 19:30:08 -0500

    se trapasa codigo desde init.lua

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,2 +1,9 @@
 -- Para que los comentarios no se repitan
 vim.cmd([[ autocmd! BufEnter * set fo-=r fo-=o ]])
+-- +--------------------------------------------------------------------+
+
+-- Colour overrides
+vim.cmd([[
+	highlight! link CursorLineNr MatchParen
+	highlight! link WinSeparator LineNr
+]])
