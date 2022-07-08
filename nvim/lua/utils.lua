7ecdedfbf44b73ee93c85ceaf0447c68cef40bac
GabrielRIP <jgrcreations@gmail.com>
2022-07-07 22:46:23 -0500

    se elimina codigo que colicionaba

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,9 +1,2 @@
 -- Para que los comentarios no se repitan
 vim.cmd([[ autocmd! BufEnter * set fo-=r fo-=o ]])
--- +--------------------------------------------------------------------+
-
--- Colour overrides
-vim.cmd([[
-	highlight! link CursorLineNr MatchParen
-	highlight! link WinSeparator LineNr
-]])
