c8c9c34420c010360d1b07956024dee0714b32b7
GabrielRIP <jgrcreations@gmail.com>
2022-07-07 22:40:58 -0500

    se persozaliza con variables y los separadores con colores

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -11,12 +11,34 @@ require('onedark').setup({
    dev = true,
 })
 -- +--------------------------------------------------------------------+
+-- Colores personalizablas
+local colors = {
+   title_nvimtree     = '#FFA630',
+   border_primary     = '#61AFEF',
+   border_gray        = '#5C6370',
+   folder_icon        = '#DAA520',
+   folder_name_close  = '#DAA520',
+   folder_name_open   = '#A0522D',
+}
 
+-- +--------------------------------------------------------------------+
 local create = vim.highlight.create
 
 create('NormalFloat', { guibg='none', guifg='none' }, false)
 create('FloatBorder', { guibg='none' }, false)
 create('WhichKeyFloat', { guibg='none' }, false)
 create('BufferTabpageFill', { guibg='none' }, false)
-create('VertSplit', { guifg='#5C6370' }, false)
-create('BufferOffset', { guifg = '#FFA630' }, true);
+-- +--------------------------------------------------------------------+
+
+-- ┌                                                                    ┐
+-- │            HACK: NvimTree Color de Folder y FolderName             │
+-- └                                                                    ┘
+create('BufferOffset', { guifg= colors.title_nvimtree }, true);
+vim.cmd('highlight NvimTreeFolderIcon guifg=' .. colors.folder_icon)
+vim.cmd('highlight NvimTreeFolderName guifg=' .. colors.folder_name_close)
+vim.cmd('highlight NvimTreeOpenedFolderName guifg=' .. colors.folder_name_open)
+vim.cmd('highlight NvimTreeIndentMarker guifg=' .. colors.border_primary)
+
+create('VertSplit', { guifg = colors.border_primary }, false)
+vim.cmd('highlight WinSeparator guibg=None guifg=' .. colors.border_primary)
+-- +--------------------------------------------------------------------+
