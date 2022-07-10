26be978ba1fd4067a0965ebff528299e8f5da1d2
GabrielRIP <jgrcreations@gmail.com>
2022-07-10 16:19:49 -0500

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,21 @@
+
 -- ┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┓
 -- ╏              init.lua by Gabriel Rivera -> GabrielRIP              ╏
--- ╏                   > nvim la mas actual y stable                    ╏
+-- ╏                 > Nvim la mas actual Stable o Dev                  ╏
 -- ╏            > Language Servers: comprobar lsp y linters             ╏
 -- ┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┛
 
+
+-- +--------------------------------------------------------------------+
 -- PERFORMANCE: Acelera la carga de modulos Lua en Nvim
 local impatient_ok, impatient = pcall(require, 'impatient')
 if impatient_ok then impatient.enable_profile() end
 -- +--------------------------------------------------------------------+
+
 -- PERFORMANCE: Para que no sea compatible con 'vi', pero no de golpe
 if vim.opt.compatible == true then vim.opt.compatible = false end
+-- +--------------------------------------------------------------------+
+
 
 -- ╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┓
 require('packer_commands')       -- Comandos para Packer & call plugins ╏
