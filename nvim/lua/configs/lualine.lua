8e6ec84b07cd0a795b26a19a350459371595bc42
GabrielRIP <jgrcreations@gmail.com>
2022-07-11 17:20:33 -0500

    se cambia el orden de git con icon fileType, filename, diagnosticos

--- nvim/lua/configs/lualine.lua
+++ nvim/lua/configs/lualine.lua
@@ -1,61 +1,61 @@
--- NOTE: modifical los colores desde aqui.
+-- HACK: modifical los colores desde aqui.
 local colors = {
-   background_1   = '#21252B',
-   background_2   = '#353535',
-   dark_bg        = '#080808',
-   text_white     = '#D0CACA',
-   icon_git       = '#F94739',
+   background_1 = '#21252B',
+   background_2 = '#353535',
+   dark_bg      = '#080808',
+   text_white   = '#D0CACA',
+   icon_git     = '#F94739',
 
    -- mode Vim
-   normal_bg      = '#DAA520',
-   normal_text    = '#080808',
+   normal_bg    = '#DAA520',
+   normal_text  = '#080808',
 
-   insert_bg      = '#569CD6',
-   insert_text    = '#080808',
+   insert_bg    = '#569CD6',
+   insert_text  = '#080808',
 
-   visual_bg      = '#79dac8',
-   visual_text    = '#080808',
+   visual_bg    = '#79dac8',
+   visual_text  = '#080808',
 
-   replace_bg     = '#DA6A73',
-   replace_text   = '#080808',
+   replace_bg   = '#DA6A73',
+   replace_text = '#080808',
 
-   command_bg     = '#98c379',
-   command_text   = '#080808',
+   command_bg   = '#98c379',
+   command_text = '#080808',
 }
 
 -- NOTE: modificado del tema 'bubbles' y 'dracula'
 local theme_custom = {
    normal = {
-      a = { fg = colors.normal_text, bg = colors.normal_bg },
-      b = { fg = colors.text_white, bg = colors.background_2 },
+      a = { fg = colors.normal_text, bg  = colors.normal_bg },
+      b = { fg = colors.text_white, bg   = colors.background_2 },
       c = { fg = colors.background_1, bg = colors.background_1 },
    },
 
-   insert = { a = { fg = colors.insert_text, bg = colors.insert_bg } },
-   visual = { a = { fg = colors.visual_text, bg = colors.visual_bg } },
+   insert  = { a = { fg = colors.insert_text, bg  = colors.insert_bg } },
+   visual  = { a = { fg = colors.visual_text, bg  = colors.visual_bg } },
    replace = { a = { fg = colors.replace_text, bg = colors.replace_bg } },
    command = { a = { fg = colors.command_text, bg = colors.command_bg } },
 
    inactive = {
-      a = { fg = colors.text_white, bg = colors.dark_bg },
-      b = { fg = colors.text_white, bg = colors.dark_bg },
+      a = { fg = colors.text_white, bg  = colors.dark_bg },
+      b = { fg = colors.text_white, bg  = colors.dark_bg },
       c = { fg = colors.normal_text, bg = colors.dark_bg },
    },
 }
 
 require('lualine').setup {
    options = {
-      theme = theme_custom,
+      theme                = theme_custom,
       component_separators = '|',
-      section_separators = { left = '', right = '' },
-      globalstatus = 3, -- para tener todos los estados de los buffers en un solo lugar, nvim 0.7
-      disabled_filetypes = { 'NvimTree'}
+      section_separators   = { left = '', right = '' },
+      globalstatus         = 3, -- Una barra de estado para todos los buffers
+      disabled_filetypes   = { 'NvimTree'}
    },
    sections = {
       lualine_a = {
          {
             'mode',
-            padding = { left = 0, right = 0 },
+            padding   = { left = 0, right   = 0 },
             separator = { left = '', right = '' },
             fmt = function(str)
                if str == 'NORMAL' then return ' ' .. str
@@ -70,32 +70,32 @@ require('lualine').setup {
          }
       },
       lualine_b = {
-         { 'branch', icon = { '', color = { fg = colors.icon_git } } },
-         {
-            'diff',
-            symbols = { added = " ", modified = " ", removed = " " }
-         },
-      },
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {
-         { 'filetype', icon_only = true, padding = { left = 0 }, separator = '' },
+         { 'filetype', icon_only = true, padding = { left = 1 }, separator = '' },
          {
             'filename',
-            file_status = false, -- sin iconos de esdos
-            path = 1, -- ruta relativa al documento principal
+            file_status = false,  -- sin iconos de esdos
+            path        = 1,      -- ruta relativa al documento principal
          },
          {
             'diagnostics',
             symbols = { error = " ", warn = " ", info = " ", hint = " " },
             update_in_insert = true,
          },
+      },
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {
+         { 'branch', icon = { '', color = { fg = colors.icon_git } } },
+         {
+            'diff',
+            symbols = { added = " ", modified = " ", removed = " " }
+         },
          { 'progress', padding = { left = 1, right = 1} }
       },
       lualine_z = {
          {
             'location',
-            padding = { left = 0, right = 0 },
+            padding   = { left = 0, right   = 0 },
             separator = { left = '', right = '' }
          },
       },
@@ -108,6 +108,6 @@ require('lualine').setup {
       lualine_y = {},
       lualine_z = { 'location' },
    },
-   tabline = {},
+   tabline    = {},
    extensions = { },
 }
