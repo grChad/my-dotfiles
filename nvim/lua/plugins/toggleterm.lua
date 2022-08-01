82d3b4a6ce72247eed74ce57a2df970d4d916f27
GabrielRIP <jgrcreations@gmail.com>
2022-08-01 13:07:34 -0500

    se cambia el teclado para usarlo

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,4 +1,4 @@
-require("toggleterm").setup{
+require("toggleterm").setup {
    -- size can be a number or function which is passed the current terminal
    size = function(term)
       if term.direction == "horizontal" then
@@ -7,7 +7,7 @@ require("toggleterm").setup{
          return vim.o.columns * 0.4
       end
    end,
-   open_mapping = [[<F12>]],
+   open_mapping = [[<F5>]],
    hide_numbers = true, -- hide the number column in toggleterm buffers
    highlights = {
       -- highlights which map to a highlight group name and a table of it's values
@@ -30,7 +30,7 @@ require("toggleterm").setup{
    start_in_insert = true,
    insert_mappings = true, -- whether or not the open mapping applies in insert mode
    persist_size = true,
-   direction = 'vertical',  -- | 'horizontal' | 'window' | 'float',
+   -- direction = 'vertical', -- | 'horizontal' | 'window' | 'float',
    close_on_exit = true, -- close the terminal window when the process exits
    shell = vim.o.shell, -- change the default shell
    -- This field is only relevant if direction is set to 'float'
@@ -47,8 +47,8 @@ require("toggleterm").setup{
 }
 
 function _G.set_terminal_keymaps()
-   local opts = {noremap = true}
-   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+   local opts = { noremap = true }
+   vim.api.nvim_buf_set_keymap(0, 't', 'kj', [[<C-\><C-n>]], opts)
    vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
    vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
    vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
