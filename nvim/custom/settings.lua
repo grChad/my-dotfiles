c4e26041f14660114fc60d1090afe24ad07ee29b
GabrielRIP <jgrcreations@gmail.com>
2022-09-16 20:45:33 -0500

    Delete: statusline = 2, colorcolum = 80

--- nvim/custom/settings.lua
+++ nvim/custom/settings.lua
@@ -1,5 +1,5 @@
 -- settings generals
-vim.g.luasnippets_path = "./lua/custom/snippets/"
+vim.g.luasnippets_path = "./lua/custom/snippets/" -- ruta de los snippets personales
 
 -- settings locals
 vim.opt.wrap = false
@@ -8,8 +8,6 @@ vim.opt.breakindent = true
 vim.opt.hidden = true
 vim.opt.writebackup = false
 vim.opt.pumheight = 10
-vim.opt.colorcolumn:append('80')
-vim.opt.laststatus = 2
 -- vim.opt.spelllang    = 'es,cjk' -- Establecer el idioma del corrector ortografico, por defecto es en 'ingles'
 -- vim.opt.spellsuggest = 'best,9' -- Muestra las 9 mejores opciones de correccion.
 vim.opt.inccommand = 'split'
@@ -22,7 +20,6 @@ map('n', '<C-Space>', '<cmd>lua vim.lsp.buf.code_action()<CR>', silent)
 map('n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', silent)
 map('v', '<Leader>ca', "<cmd>'<,'>lua vim.lsp.buf.range_code_action()<CR>", silent)
 map('n', '<Leader>re', "<cmd>lua vim.lsp.buf.rename()<CR>", silent)
--- map('n', '<Leader>cf', "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", silent)
 map('n', '<C-p>', "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", silent)
 map('v', '<C-p>', "<cmd>'<.'>lua vim.lsp.buf.range_formatting()<CR>", silent)
 map('n', 'K', "<cmd>lua vim.lsp.buf.hover()<CR>", silent)
