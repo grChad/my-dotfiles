f221a96e18f929ad6a7f00db0968d80f7ba6c832
GabrielRIP <jgrcreations@gmail.com>
2022-09-30 17:13:45 -0500

    Add: nueva configuracio y indentado

--- nvim/custom/configs.lua
+++ nvim/custom/configs.lua
@@ -2,12 +2,12 @@ local icons = require("custom.iconos")
 local colors = require("custom.theme.colors")
 
 return {
-	NvimTree = {
-		windows_float = true,
-		width = 32,
-		height = 25,
-		position = "left", -- left and right
-	},
-	icons = icons,
-	colors = colors,
+   NvimTree = {
+      windows_float = true,
+      width = 40,
+      height = 25,
+      position = "left", -- if windows_float =  false: => 'left' and 'right'
+   },
+   icons = icons,
+   colors = colors,
 }
