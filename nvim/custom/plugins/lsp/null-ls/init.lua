c78eabfe3fee79ea2c0ff53262917e744400403b
GabrielRIP <jgrcreations@gmail.com>
2022-10-02 00:11:22 -0500

    Disabled: eslint_d, hasta nuevo aviso

--- nvim/custom/plugins/lsp/null-ls/init.lua
+++ nvim/custom/plugins/lsp/null-ls/init.lua
@@ -9,16 +9,16 @@ local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 null_ls.setup({
    debug = true,
    sources = {
-      null_ls.builtins.diagnostics.eslint_d.with({
-         diagnostics_format = "[eslint] #{m}\n(#{c})",
-         -- ignore prettier warnings from eslint-plugin-prettier
-         filter = function(diagnostic)
-            return diagnostic.code ~= "prettier/prettier"
-         end,
-         condition = function(utils)
-            return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json", ".eslintrc.yaml", ".eslintrc" })
-         end,
-      }),
+      -- null_ls.builtins.diagnostics.eslint_d.with({
+      --    diagnostics_format = "[eslint] #{m}\n(#{c})",
+      --    -- ignore prettier warnings from eslint-plugin-prettier
+      --    filter = function(diagnostic)
+      --       return diagnostic.code ~= "prettier/prettier"
+      --    end,
+      --    condition = function(utils)
+      --       return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json", ".eslintrc.yaml", ".eslintrc" })
+      --    end,
+      -- }),
       null_ls.builtins.formatting.stylua.with({
          condition = function(utils)
             return utils.root_has_file({ "stylua.toml", ".stylua.toml" })
@@ -41,15 +41,15 @@ null_ls.setup({
       }),
    },
    on_attach = function(client, bufnr)
-   	if client.server_capabilities.documentFormattingProvider then
-   		vim.api.nvim_clear_autocmds({ buffer = 0, group = augroup_format })
-   		vim.api.nvim_create_autocmd("BufWritePre", {
-   			group = augroup_format,
-   			buffer = 0,
-   			callback = function()
-   				vim.lsp.buf.format({ timeout_ms = 2000 })
-   			end,
-   		})
-   	end
+      if client.server_capabilities.documentFormattingProvider then
+         vim.api.nvim_clear_autocmds({ buffer = 0, group = augroup_format })
+         vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup_format,
+            buffer = 0,
+            callback = function()
+               vim.lsp.buf.format({ timeout_ms = 2000 })
+            end,
+         })
+      end
    end,
 })
