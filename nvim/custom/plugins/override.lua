e3dcbc6db57f32254b370a6e7633e8ccd5ae1ee0
GabrielRIP <jgrcreations@gmail.com>
2022-10-02 00:11:48 -0500

    Delete: codigo anterior de treesitter

--- nvim/custom/plugins/override.lua
+++ nvim/custom/plugins/override.lua
@@ -1,61 +1,47 @@
 local M = {}
 
-M.treesitter = {
-	ensure_installed = "all",
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = true,
-	},
-	indent = { enable = true },
-	autotag = { enable = true },
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
-}
-
 M.uiChad = {
-	tabufline = {
-		enabled = false,
-		lazyload = true,
-		overriden_modules = nil,
-	},
+   tabufline = {
+      enabled = false,
+      lazyload = true,
+      overriden_modules = nil,
+   },
 }
 
 local function border(hl_name)
-	return {
-		{ "╭", hl_name },
-		{ "─", hl_name },
-		{ "╮", hl_name },
-		{ "│", hl_name },
-		{ "╯", hl_name },
-		{ "─", hl_name },
-		{ "╰", hl_name },
-		{ "│", hl_name },
-	}
+   return {
+      { "╭", hl_name },
+      { "─", hl_name },
+      { "╮", hl_name },
+      { "│", hl_name },
+      { "╯", hl_name },
+      { "─", hl_name },
+      { "╰", hl_name },
+      { "│", hl_name },
+   }
 end
 
 M.cmp_change = {
-	window = {
-		completion = {
-			border = border("FloatBorder"),
-			winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
-		},
-		documentation = {
-			border = border("FloatBorder"),
-			winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-		},
-	},
-	experimental = {
-		ghost_text = true,
-	},
+   window = {
+      completion = {
+         border = border("FloatBorder"),
+         winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
+      },
+      documentation = {
+         border = border("FloatBorder"),
+         winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+      },
+   },
+   experimental = {
+      ghost_text = true,
+   },
 }
 
 M.git_signs = {
-	signs = {
-		add = { hl = "GitSignsAdd", text = "┃", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "┃", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-	},
+   signs = {
+      add = { hl = "GitSignsAdd", text = "┃", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+      change = { hl = "GitSignsChange", text = "┃", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+   },
 }
 
 return M
