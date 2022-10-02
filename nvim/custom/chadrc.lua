38feddbea6bc94a85d09cf9f0648bafff78a2e81
GabrielRIP <jgrcreations@gmail.com>
2022-10-02 00:09:02 -0500

    Remove: plugin treesitter de NvChad

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -6,21 +6,20 @@ local M = {}
 -- example of changing theme:
 
 M.ui = {
-	theme = "doomchad",
-	hl_override = require("custom.theme.hl_override"),
-	hl_add = require("custom.theme.hl_add"),
+   theme = "doomchad",
+   hl_override = require("custom.theme.hl_override"),
+   hl_add = require("custom.theme.hl_add"),
 }
 
 local pluginConfs = require("custom.plugins.override")
 
 M.plugins = {
-	remove = {},
-	user = require("custom.plugins"),
-	override = {
-		["nvim-treesitter/nvim-treesitter"] = pluginConfs.treesitter,
-		["hrsh7th/nvim-cmp"] = pluginConfs.cmp_change,
-		["lewis6991/gitsigns.nvim"] = pluginConfs.git_signs,
-	},
+   remove = { "nvim-treesitter/nvim-treesitter" },
+   user = require("custom.plugins"),
+   override = {
+      ["hrsh7th/nvim-cmp"] = pluginConfs.cmp_change,
+      ["lewis6991/gitsigns.nvim"] = pluginConfs.git_signs,
+   },
 }
 
 M.mappings = require("custom.mappings")
