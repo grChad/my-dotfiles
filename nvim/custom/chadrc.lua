71a991ed2fea81ae236eaa2cf579679a7a616f8a
GabrielRIP <jgrcreations@gmail.com>
2022-11-26 19:33:08 -0500

    Add: Modificar grupos destacados de Nvim y treesitter

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -1,14 +1,13 @@
 local M = {}
 
 M.ui = {
-   theme = "doomchad",
-   hl_override = require("custom.theme.hl_override"),
-   hl_add = require("custom.theme.hl_add"),
+	theme = "doomchad",
+	hl_override = require("custom.theme.hl_override"),
+	hl_add = require("custom.theme.hl_add"),
 }
 
 -- configuración de plugins: remove, overrides and new plugins
 M.plugins = require("custom.plugins")
-M.plugins.remove = { "nvim-treesitter/nvim-treesitter" } -- provicional
 
 -- configuración de atajos de teclado
 M.mappings = require("custom.mappings")
