cc7de6757ba6310e72fbc106c717f994c762ec9e
GabrielRIP <jgrcreations@gmail.com>
2022-11-26 19:27:23 -0500

    Add: settigs scrolloff y sedescrolloff

--- nvim/custom/settings.lua
+++ nvim/custom/settings.lua
@@ -13,6 +13,8 @@ local options = {
    spellsuggest = "best,9", -- Muestra las 9 mejores opciones de correccion.
    spelloptions = "camel", -- Para que no muestre error ortografico en los CamelCase
    inccommand = "split",
+   scrolloff = 5, -- Dejar espacio encima o debajo del cursor
+   sidescrolloff = 8, -- Dejar espacio a los laterales del cursor
 }
 
 for key, value in pairs(options) do
