8d4809eb3309bc0be4d802be4a1c2893b668b62b
grChad <jgrcreations@gmail.com>
2023-08-23 17:33:10 -0500

    ✨ feat: se guarda una configuración de un plugin

---
+++ nvim/blankline.lua
@@ -0,0 +1,21 @@
+local blankline = {
+   indentLine_enabled = 1,
+   filetype_exclude = {
+      "help",
+      "terminal",
+      "alpha",
+      "lazy",
+      "lspinfo",
+      "TelescopePrompt",
+      "TelescopeResults",
+      "mason",
+      "nvdash",
+      "nvcheatsheet",
+      "",
+   },
+   char = "▏",
+   buftype_exclude = { "terminal" },
+   show_trailing_blankline_indent = false,
+   show_current_context = true,
+   show_current_context_start = true,
+}
